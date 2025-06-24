935c521d1c847ce3c86da45e5feb0c8995747a56
Gastón Franco <gastonfrancorocha@gmail.com>
2025-06-24 16:15:23 -0300

    fix: Home como Client Component definitivo

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
-"use client"; // 👈 lo agregás arriba de todo
+"use client"; // 👈 muy importante, sin esto no sirve
 
 import { useEffect, useState } from "react";
 import { personalData } from "@/utils/data/personal-data";
+
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
@@ -30,7 +31,7 @@ export default function Home() {
       <AboutSection />
       <Experience />
       <Skills />
-      <Projects />
+      <Projects blogs={blogs} /> {/* Si pasás blogs al componente */}
       <Education />
       <ContactSection />
     </div>
