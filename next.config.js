84f2f1d54cbb11daab17421a2ce198f527004568
Gastón Franco <gastonfrancorocha@gmail.com>
2025-06-24 17:29:57 -0300

    lpm

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  // Ya no ponemos `output: 'export'` para que Vercel use SSR/ISR normalmente
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
-    unoptimized: true, // 💥 Obligatorio si usás <Image> en export
+    unoptimized: true, // útil si usás <Image> en cliente
     remotePatterns: [
       {
         protocol: 'https',
