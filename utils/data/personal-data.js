12c097d8b36a58e55e6552329ef9f33f6248a03d
Gastón Franco <gastonfrancorocha@gmail.com>
2025-06-25 00:11:50 -0300

    cambio direccion

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "My name is Gastón Franco. I am a computer engineer student and an enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'gastonfrancorocha@gmail.com',
   phone: '+59898397799',
-  address: 'Quijote 2631, Montevideo, Uruguay',
+  address: 'Montevideo, Uruguay',
   github: 'https://github.com/gastonfr11',
   facebook: '',
   linkedIn: 'https://www.linkedin.com/in/gastonfranco11/',
